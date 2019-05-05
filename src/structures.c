ffebc485809d603ae75dc6f2f647ab405f969a95
ryangibb <ryangibb321@gmail.com>
2019-05-05 12:47:17 +0100

    populate world

--- src/structures.c
+++ src/structures.c
@@ -1,64 +1,110 @@
 #include <stdbool.h>
 #include <stdlib.h>
+#include <time.h>
 
 #include "structures.h"
 
-World* make_world(int x_size, int y_size, int num_carns, int num_herbs, int num_food) {
-  (void) num_carns;
-  (void) num_herbs;
-  (void) num_food;
-  int map_size = sizeof(Tile*) * y_size + sizeof(Tile) * x_size * y_size;
-  Tile*** map = malloc(map_size);
-  Tile** first_tile = (Tile**) map + y_size; // pointer to first Tile pointer, in 2D array
+const int array_terminator = -1;
 
-  //   global tiles
-  // global start_prey
-  // global start_predators
-  // global start_food
-  // global growth
-  // tiles = int(x_size * y_size)
-  // start_prey = int(tiles * start_prey_per_tile)
-  // start_predators = int(tiles * start_predators_per_tile)
-  // start_food = int(tiles * start_food_per_tile)
-  // growth = int(tiles * growth_per_tile)
-  //
-  // global world
-  // global predators
-  // global prey
-  // global no_food
-  // world = [[Tile() for x in range(x_size)] for y in range(y_size)]
-  // predators = [None] * start_predators
-  // prey = [None] * start_prey
-  // no_creatures = [(x, y) for x in range(x_size) for y in range(y_size)]
-  // no_food = [(x, y) for x in range(x_size) for y in range(y_size)]
-  // for i in range(start_predators):
-  //     x, y = random.choice(no_creatures)
-  //     world[y][x].entity = Predator(starting_health)
-  //     predators[i] = (x, y)
-  //     no_creatures.remove((x, y))
-  // for i in range(start_prey):
-  //     x, y = random.choice(no_creatures)
-  //     world[y][x].entity = Prey(starting_health)
-  //     prey[i] = (x, y)
-  //     no_creatures.remove((x, y))
-  // for i in range(start_food):
-  //     x, y = random.choice(no_food)
-  //     world[y][x].food = True
-  //     no_food.remove((x, y))
-  // run()
+// Returns -1 if not found, or integer position if found
+int val_in_array(int arr[], int val) {
+  int i = 0;
+  while (arr[i] != array_terminator) {
+    if (arr[i] == val) {
+      return i;
+    }
+    i++;
+  }
+  return -1;
+}
+
+void append_array(int arr[], int val, int pos) {
+  arr[pos] = val;
+  arr[pos + 1] = array_terminator;
+}
+
+World* make_world(Point dimensions, int start_carns, int start_herbs, int start_food, int starting_health) {
+  PointNode predator_points = NULL;
+
+
+
+  int tiles_size = sizeof(Tile*) * y_size + sizeof(Tile) * x_size * y_size;
+  Tile*** tiles = malloc(tiles_size);
+  Tile** first_tile = (Tile**) tiles + y_size; // pointer to first Tile pointer, in 2D array
   for (int y = 0; y < y_size; y++) {
     // point y column pointer to appropriate location in 2D array (to start of x row)
-    map[y] = (first_tile + x_size * y);
+    tiles[y] = (first_tile + x_size * y);
     for (int x = 0; x < x_size; x++) {
-      // Creature* creature = make_creature(false, Herb, 0);
-      Tile* tile = make_tile(NULL, false);
-      map[y][x] = tile;
+      tiles[y][x] = make_tile();
     }
   }
+  int tiles_to_populate = start_carns + start_herbs + start_food;
+  int used_x[tiles_to_populate + 1], used_y[tiles_to_populate + 1]; // plus one for array_terminator
+  used_y[0] = array_terminator;
+  used_x[0] = array_terminator;
+  int len_used_y = 0;
+  int len_used_x = 0;
+
+  srand(time(0)); // Use current time as seed for random generator
+  for (int i = 0; i < start_carns; i++) {
+    int y = rand() % y_size;
+    int x = rand() % x_size;
+    while (
+        val_in_array(used_y, y) != -1 &&
+        val_in_array(used_x, x) != -1
+      ) {
+      y = rand() % y_size;
+      x = rand() % x_size;
+    }
+    append_array(used_y, y, len_used_y);
+    len_used_y++;
+    append_array(used_x, x, len_used_x);
+    len_used_x++;
+    tiles[y][x]->type = Carn;
+    Creature carn = {starting_health};
+    tiles[y][x]->creature = carn;
+  }
+
+  for (int i = 0; i < start_herbs; i++) {
+    int y = rand() % y_size;
+    int x = rand() % x_size;
+    // while (
+    //     val_in_array(used_y, y) != -1 &&
+    //     val_in_array(used_x, x) != -1
+    //   ) {
+    //   y = rand() % y_size;
+    //   x = rand() % x_size;
+    // }
+    append_array(used_y, y, len_used_y);
+    len_used_y++;
+    append_array(used_x, x, len_used_x);
+    len_used_x++;
+    tiles[y][x]->type = Herb;
+    Creature herb = {starting_health};
+    tiles[y][x]->creature = herb;
+  }
+
+  for (int i = 0; i < start_food; i++) {
+    int y = rand() % y_size;
+    int x = rand() % x_size;
+    while (
+        val_in_array(used_y, y) != -1 &&
+        val_in_array(used_x, x) != -1
+      ) {
+        y = rand() % y_size;
+        x = rand() % x_size;
+    }
+    append_array(used_y, y, len_used_y);
+    len_used_y++;
+    append_array(used_x, x, len_used_x);
+    len_used_x++;
+    tiles[y][x]->food = true;
+  }
+
   World* world = malloc(sizeof(World));
   world->x_size = x_size;
   world->y_size = y_size;
-  world->map = map;
+  world->tiles = tiles;
   return world;
 }
 
@@ -66,39 +112,24 @@ void free_world(World* world) {
   for (int y = 0; y < world->y_size; y++) {
     for (int x = 0; x < world->x_size; x++) {
       // TODO is this needed?
-      if (world->map[x][y] != NULL) {
-        free_tile(world->map[x][y]);
+      if (world->tiles[x][y] != NULL) {
+        free_tile(world->tiles[x][y]);
       }
     }
   }
-  free(world->map);
+  free(world->tiles);
   free(world);
 }
 
-Tile* make_tile(Creature* creature, bool food) {
+Tile* make_tile() {
   Tile* tile = malloc(sizeof(Tile));
-  tile->creature = creature;
-  tile->food = food;
+  tile->type = None;
+  Creature none_creature = {0};
+  tile->creature = none_creature;
+  tile->food = false;
   return tile;
 }
 
 void free_tile(Tile* tile) {
-  // TODO is this needed?
-  if (tile->creature != NULL) {
-    free_creature(tile->creature);
-  }
   free(tile);
-}
-
-Creature* make_creature(bool exists, Type type, float health) {
-  Creature* creature = malloc(sizeof(Creature));
-  creature->exists = exists;
-  // creature->loc = loc;
-  creature->type = type;
-  creature->health = health;
-  return creature;
-}
-
-void free_creature(Creature* creature) {
-  free(creature);
 }
